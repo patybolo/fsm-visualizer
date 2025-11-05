4a1498d83917a85fd3e2e954c28ff525b3aab8db
patybolo <57047110+patybolo@users.noreply.github.com>
2025-11-04 23:13:05 -0300

    update eraser

--- main.c
+++ main.c
@@ -3,7 +3,14 @@
 
 #include <stdbool.h>
 
+#define BMP_WIDTH 240
+#define BMP_HEIGHT 120
+#define BMP_MAX_PIX (BMP_WIDTH * BMP_HEIGHT)
+
 TPixel defPix = {0xff, 0xff, 0xff, 255};
+TPixel erasePix = {0x9c, 0x97, 0xbd, 255};
+
+bool isErasing = false;
 
 typedef struct {
     int x;
@@ -11,6 +18,7 @@ typedef struct {
 } t_pix;
 
 int update_text_buffer(Tigr*, int, char*);
+void fast_update_bmp(Tigr*, int, int);
 
 int main(int argc, char *argv[])
 {
@@ -21,29 +29,16 @@ int main(int argc, char *argv[])
     t_pix end_pix;
     int buttons;
     bool ready_draw = false;
-    Tigr *screen = tigrWindow(240, 120, "FSM VISUALIZER", 3);
+    Tigr *screen = tigrWindow(240, 120, "FSM VISUALIZER", 0);
     float elapsed_time = tigrTime();
     tigrClear(screen, tigrRGB(0x9c, 0x97, 0xbd));
 
     while (!tigrClosed(screen))
     {
-    
         //tigrPrint(screen, tfont, 0, 0, tigrRGB(0xff, 0xff, 0xff), text_buffer);
-
         tigrMouse(screen, &pix.x, &pix.y, &buttons);
         if(buttons == 1) {
-            tigrPlot(screen, pix.x, pix.y, defPix);
-        }
-
-        if(buttons == 4 && !ready_draw) {
-            start_pix = pix;
-            elapsed_time = tigrTime();
-            ready_draw = true;
-        }
-        else if(buttons == 4 && ready_draw && tigrTime() >= elapsed_time + 0.01f) {
-            end_pix = pix;
-            tigrLine(screen, start_pix.x,start_pix.y, end_pix.x, end_pix.y, defPix);
-            ready_draw = false;
+            fast_update_bmp(screen, pix.x, pix.y);
         }
 
         curr_char = update_text_buffer(screen, curr_char, text_buffer);
@@ -59,10 +54,20 @@ int update_text_buffer(Tigr *screen, int curr_char, char *buffer)
         tigrSaveImage("draw.png", screen);
     if(c == 'c')
         tigrClear(screen, tigrRGB(0x9c, 0x97, 0xbd));
+    if(c == 'b')
+        isErasing = !isErasing;
     if (c != NULL)
         buffer[++curr_char] = c;
     if (curr_char % 90 == 0)
         buffer[curr_char] = '\n';
     
     return curr_char;
+}
+
+void fast_update_bmp(Tigr* bmp, int x, int y) {
+    if(y * BMP_WIDTH + x > BMP_MAX_PIX) return;
+    if(!isErasing)
+        bmp->pix[y * BMP_WIDTH + x] = defPix;
+    else
+        bmp->pix[y * BMP_WIDTH + x] = erasePix;
 }
